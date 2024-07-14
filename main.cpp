3dcd8f604707b52e831a4e6d3a7fdb85c0cf2822
Andrei <grigormaklaud@gmail.com>
2024-07-14 12:55:08 +0300

    add giyhub

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main() {
   auto context = async::connect();
+  auto context_2 = async::connect();
   async::receive(context, "1", 1);
+  async::receive(context, "123\n2123\n3333\n4333\n5333\n6333\n", 6);
+  async::receive(context_2, "abc\nacc\nabc\nabc\nabc\n", 5);
   async::disconnect(context);
-
-  std::this_thread::sleep_for(std::chrono::minutes(1));
+  async::disconnect(context_2);
 
   return 0;
 }
